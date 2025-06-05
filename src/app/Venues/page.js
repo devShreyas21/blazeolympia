3efa8ed5c4e11a87b44801e41062bee3e7628d3d
Shreyask21-dev <shreyas.k@coinage.in>
2025-06-05 20:31:29 +0530

    Update page.js

--- src/app/Venues/page.js
+++ src/app/Venues/page.js
@@ -2,6 +2,8 @@ import React from 'react'
 import styles from "../page.module.css";
 import Image from 'next/image';
 export default function page() {
+
+  
   return (
     <>
       <main className="main">
