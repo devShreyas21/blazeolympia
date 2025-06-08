3293c6b28b51e8edfc3da3a0a2a1396eba85c623
Shreyas Kshirsagar <137375050+devShreyas21@users.noreply.github.com>
2025-06-08 10:39:51 +0530

    Update page.js

--- src/app/About/page.js
+++ src/app/About/page.js
@@ -3,10 +3,7 @@ import styles from "../page.module.css";
 import Image from 'next/image';
 export default function Page() {
   return (
-    <>
-
-
-    
+    <>    
       <main className="main">
         {/* <!-- Page Title --> */}
         <div
